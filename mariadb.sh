6cf7b26ca88358b12414bd7bd42d9c373ec5e2a7
Phillip McKeown <phillip.mckeown@outlook.com>
2020-12-15 10:20:55 +0000

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -28,11 +28,9 @@ fi
 
 echo "Install MariaDB 10.4"
 sudo apt -y install software-properties-common
-
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-
 sudo add-apt-repository "deb [arch=amd64,arm64,ppc64el] http://mariadb.mirror.liquidtelecom.com/repo/10.4/ubuntu $(lsb_release -cs) main"
-
+sudo apt -y update
 sudo apt -y install mariadb-server mariadb-client
 
 echo "Config MariaDB"
