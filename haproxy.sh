c7e5f1c4ba2b7d3e40424a61b6df38a8bfb879e3
Phillip McKeown <phillip.mckeown@outlook.com>
2020-12-17 14:06:01 +0000

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -21,46 +21,51 @@ sudo cp /etc/haproxy/haproxy.cfg{,.original}
 echo "Create default config"
 sudo bash -c 'cat > /etc/haproxy/haproxy.cfg' << EOF
 global
-  log /dev/log    local0 notice
-  log /dev/log    local1 notice
-  chroot /var/lib/haproxy
-  stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
-  stats timeout 30s
-  user haproxy
-  group haproxy
-  daemon
+      log /dev/log    local0 notice
+      log /dev/log    local1 notice
+      chroot /var/lib/haproxy
+      stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
+      stats timeout 30s
+      user haproxy
+      group haproxy
+      daemon
 
 defaults
-  log     global
-  option  httplog
-  option  dontlognull
-  timeout connect 5000
-  timeout client  50000
-  timeout server  50000
-  errorfile 400 /etc/haproxy/errors/400.http
-  errorfile 403 /etc/haproxy/errors/403.http
-  errorfile 408 /etc/haproxy/errors/408.http
-  errorfile 500 /etc/haproxy/errors/500.http
-  errorfile 502 /etc/haproxy/errors/502.http
-  errorfile 503 /etc/haproxy/errors/503.http
-  errorfile 504 /etc/haproxy/errors/504.http
+      log     global
+      option  httplog
+      option  dontlognull
+      timeout connect 5000
+      timeout client  50000
+      timeout server  50000
+      errorfile 400 /etc/haproxy/errors/400.http
+      errorfile 403 /etc/haproxy/errors/403.http
+      errorfile 408 /etc/haproxy/errors/408.http
+      errorfile 500 /etc/haproxy/errors/500.http
+      errorfile 502 /etc/haproxy/errors/502.http
+      errorfile 503 /etc/haproxy/errors/503.http
+      errorfile 504 /etc/haproxy/errors/504.http
         
 EOF
 
 if [[ "$PROXY_TYPE" = "sql" ]]; then 
 sudo bash -c 'cat >> /etc/haproxy/haproxy.cfg' << EOF
 listen master
-  bind :3306
-  mode tcp
-  balance roundrobin
+      bind :3306
+      mode tcp
+      option mysql-check user mysql_check
+      default-server check
+      # server master 10.0.0.0:3306 check
         
         
 listen slave
-  bind :3307
-  mode tcp
-  option httpchk
-  balance roundrobin
-
+      bind :3307
+      mode tcp
+      option mysql-check user mysql_check
+      option httpchk
+      balance roundrobin
+      default-server check port 9876 on-marked-down shutdown-sessions
+      # server slave-1 10.0.0.0:3306 
+      
 EOF
 fi
 
