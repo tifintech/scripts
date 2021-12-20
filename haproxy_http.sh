99021c69eab095b99724a510b803161a237e33a0
Phillip McKeown <phillip@viodev.com>
2021-12-20 16:06:29 +0000

    Update haproxy_http.sh

--- haproxy_http.sh
+++ haproxy_http.sh
@@ -1,6 +1,6 @@
 echo "Install HAProxy 2.1"
 sudo apt -y install software-properties-common
-sudo add-apt-repository ppa:vbernat/haproxy-2.1 --yes
+sudo add-apt-repository ppa:vbernat/haproxy-2.4 -y
 sudo apt -y update
 sudo apt -y install haproxy
 
